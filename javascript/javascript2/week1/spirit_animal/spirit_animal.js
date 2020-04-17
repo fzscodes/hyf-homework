685e255861614542a0ae871996910d2ab861386f
Fauzia Siddique <FauziaSiddique@HackYourFutures-MacBook-Pro.local>
2020-04-17 13:48:19 +0200

    implemented feedback

--- javascript/javascript2/week1/spirit_animal/spirit_animal.js
+++ javascript/javascript2/week1/spirit_animal/spirit_animal.js
@@ -1,13 +1,13 @@
 const spiritAnimal = ["Alert Hawk", "Power Horse", "Pride Lion", "Social Monkey", "Grace Ostrich", "Wise Owl", "Mystical Panther", "Magical Raven", "Patient Turtle", "Gracefull Swan", "Brave wolf"];
 function getSpiritAnimal() {
-    let inputName = document.getElementById("name").value;
+    const inputName = document.getElementById("name").value;
     if(!inputName)
     {
         alert('Enter a name first!');
     }
     else{
-    let randomNumber = Math.floor(Math.random()*spiritAnimal.length);
-    let randomSpiritAnimal = spiritAnimal[randomNumber];
+    const randomNumber = Math.floor(Math.random()*spiritAnimal.length);
+    const randomSpiritAnimal = spiritAnimal[randomNumber];
     document.getElementById("spirit-animal").textContent =inputName + "-" + "The " + randomSpiritAnimal;
     }
 }
