d6636ac2b14057cdc92935600e5774c19bc82c3e
Fauzia Siddique <FauziaSiddique@hackyouturesmbp.home>
2020-04-19 00:38:40 +0200

    added creditCardNo files

---
+++ javascript/javascript1/week4/creditCardNo/ccNo.js
@@ -0,0 +1,13 @@
+function formatCreditCardNumber(number) {
+    let numberToString = number.toString();
+    const gap_size = 4;
+    let result = "";
+    while (numberToString.length > 0) // Loop through string
+    {
+        result = result + " " + numberToString.substring(0, gap_size); // Insert space character
+        number = numberToString.substring(gap_size);  // Trim String
+    }
+    return result;
+}
+const formattedCreditCardObject = formatCreditCardNumber(123456789);
+console.log(formattedCreditCardObject);
