b07f44d269af32c631cdcc1a8c7fc03b390d933b
Fauzia Siddique <FauziaSiddique@hackyouturesmbp.home>
2020-04-18 23:56:07 +0200

    implemented review

--- javascript/javascript2/week3/functionAsVariable/functionAsVariable.js
+++ javascript/javascript2/week3/functionAsVariable/functionAsVariable.js
@@ -8,17 +8,17 @@ const arrayOfFunctions = [
 arrayOfFunctions.forEach(element => element());
 
 //Create a function as a const and try creating a function normally. Call both functions.
- const name = function nameFinder(name){
-     console.log("Have a nice day " + name);
- }
+const name = function (name) {
+    console.log("Have a nice day " + name);
+}
 
- function nameLogger(name){
-     console.log("Your name is: "+ name);
- }
- name("Fauzia");
- nameLogger("Fauzia");
+function nameLogger(name) {
+    console.log("Your name is: " + name);
+}
+name("Fauzia");
+nameLogger("Fauzia");
 
- //Create an object that has a key whose value is a function. Try calling this function
-  const object = { key : () => console.log("I am a function")};
+//Create an object that has a key whose value is a function. Try calling this function
+const object = { key: () => console.log("I am a function") };
 
-  object.key();
+object.key();
